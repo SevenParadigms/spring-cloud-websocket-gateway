81339fb9ecd9a4bd73b603f9fad3e6e13e767f33
Lao Tsing <lao.tsing@outlook.com>
2022-04-04 03:39:15 +0300

    validate token only by public key

--- src/main/kotlin/io/github/sevenparadigms/gateway/configuration/WebSocketConfiguration.kt
+++ src/main/kotlin/io/github/sevenparadigms/gateway/configuration/WebSocketConfiguration.kt
@@ -1,18 +1,18 @@
 package io.github.sevenparadigms.gateway.configuration
 
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration
+import io.github.sevenparadigms.abac.configuration.SecurityConfig
+import io.github.sevenparadigms.gateway.websocket.support.WebsocketHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.r2dbc.config.Beans
+import org.springframework.context.annotation.Import
 import org.springframework.web.reactive.HandlerMapping
 import org.springframework.web.reactive.socket.server.WebSocketService
 import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter
 import org.springframework.web.reactive.socket.server.upgrade.ReactorNettyRequestUpgradeStrategy
-import io.github.sevenparadigms.gateway.websocket.support.WebsocketHandler
 
 @Configuration
-@ImportAutoConfiguration(Beans::class)
+@Import(SecurityConfig::class)
 class WebSocketConfiguration {
     @Bean
     fun handlerMapping(): HandlerMapping = WebsocketHandler()
