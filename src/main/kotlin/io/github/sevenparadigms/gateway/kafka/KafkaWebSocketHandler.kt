dad678137756598d29d1913c5023f7deadb0daf8
Lao Tsing <lao.tsing@outlook.com>
2022-05-07 19:41:24 +0300

    update depends

--- src/main/kotlin/io/github/sevenparadigms/gateway/kafka/KafkaWebSocketHandler.kt
+++ src/main/kotlin/io/github/sevenparadigms/gateway/kafka/KafkaWebSocketHandler.kt
@@ -2,8 +2,8 @@ package io.github.sevenparadigms.gateway.kafka
 
 import io.github.sevenparadigms.gateway.kafka.model.EventWrapper
 import io.github.sevenparadigms.gateway.kafka.model.WebSocketEvent
-import org.sevenparadigms.kotlin.common.error
 import org.sevenparadigms.kotlin.common.jsonToObject
+import org.sevenparadigms.kotlin.common.severe
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -13,5 +13,5 @@ class KafkaHandler(private val kafkaPublisher: EventDrivenPublisher) {
     fun publish(request: ServerRequest) = request.bodyToMono(EventWrapper::class.java)
         .flatMap { kafkaPublisher.publish(it.topic, it.body.jsonToObject(WebSocketEvent::class.java)) }
         .flatMap { ServerResponse.ok().build() }
-        .doOnError { error("Exception while trying to process event: " + it.message) }
+        .doOnError { severe { "Exception while trying to process event: " + it.message } }
 }
